3d53a0475977117b6c371a99291c0cbb5140996e
Manoranjan <sahoomanoranjan626@gmail.com>
2023-03-29 19:51:32 +0530

    Added test cases for header component

---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Login from "../../../pages/Login/Login";
+
+describe("Test the login Page", () => {
+  test("render the login with a button", async () => {
+    render(<Login />);
+    const btn = await screen.findAllByRole("button");
+    expect(btn).toHaveLength(1);
+  });
+
+  test("Validate email validate", () => {
+    // const email = "sagar.com";
+  });
+});
