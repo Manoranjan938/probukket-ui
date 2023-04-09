4482c8e7eb27cb4899ae8dbdae6dbcac0f3d5457
Manoranjan <sahoomanoranjan626@gmail.com>
2023-04-09 13:47:13 +0530

    Added animation for signup btn and added time ago code

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,23 @@
-import { type ReactElement } from "react";
+import type { ReactNode, ReactElement } from "react";
 import "./style.css";
 
 interface ButtonProps {
   title: string;
   classes: string;
+  children?: ReactNode;
+  onClick?: () => void;
 }
 
-const Button = ({ title, classes }: ButtonProps): ReactElement => {
+const Button = ({
+  title,
+  classes,
+  children,
+  onClick,
+}: ButtonProps): ReactElement => {
   return (
-    <button type="button" className={classes}>
-      {title}
+    <button type="button" className={classes} onClick={onClick}>
+      {children ? <div className="btn-icon">{children}</div> : null}
+      <span className="btn-title">{title}</span>
     </button>
   );
 };
