6dc49cdc9c31a6e36f715f7c76c57d5081f4488d
Manoranjan <sahoomanoranjan626@gmail.com>
2023-05-02 23:41:52 +0530

    Added task design

--- src/pages/Dashboard/pages/Tasks/components/Boards/index.tsx
+++ src/pages/Dashboard/pages/Tasks/components/Boards/index.tsx
@@ -1,10 +1,39 @@
 import { type ReactElement } from "react";
 
+import { BsThreeDots } from "react-icons/bs";
+import "./style.css";
+
 const BoardView = (): ReactElement => {
   return (
     <section className="board_view_section">
       <div className="board_view_header">
-        <div className="board_view"></div>
+        <div className="task_board">
+          <div className="board_details">
+            <span className="header_title">To Do</span>
+            <span className="counter">2 Tasks</span>
+          </div>
+          <div className="board_icon">
+            <BsThreeDots />
+          </div>
+        </div>
+        <div className="task_board">
+          <div className="board_details">
+            <span className="header_title">To Do</span>
+            <span className="counter">2 Tasks</span>
+          </div>
+        </div>
+        <div className="task_board">
+          <div className="board_details">
+            <span className="header_title">To Do</span>
+            <span className="counter">2 Tasks</span>
+          </div>
+        </div>
+        <div className="task_board">
+          <div className="board_details">
+            <span className="header_title">To Do</span>
+            <span className="counter">2 Tasks</span>
+          </div>
+        </div>
       </div>
     </section>
   );
