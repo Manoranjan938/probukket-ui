89102eb2438a8d9f1e2393157a8877656a37776d
Manoranjan <sahoomanoranjan626@gmail.com>
2023-05-03 23:25:44 +0530

    Added some new design for task design

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,11 @@
+import { type ReactElement } from "react";
+
+const Menu = (): ReactElement => {
+  return (
+    <div className="menu">
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default Menu;
