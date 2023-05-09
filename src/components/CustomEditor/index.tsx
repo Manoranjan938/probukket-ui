1725fc4519b17faae18d2193084f6b6e5ba02c04
Manoranjan <sahoomanoranjan626@gmail.com>
2023-05-09 23:20:53 +0530

    Something added

---
+++ src/components/CustomEditor/index.tsx
@@ -0,0 +1,11 @@
+import React, { type ReactElement } from "react";
+
+const Editor = (): ReactElement => {
+  return (
+    <section className="editor_section">
+      <h1>Hello</h1>
+    </section>
+  );
+};
+
+export default Editor;
