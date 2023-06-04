1df1344647ed2bc8a47a91ef9e1e1d48edaffd7a
Manoranjan <sahoomanoranjan626@gmail.com>
2023-06-04 23:55:39 +0530

    Added lazy loading and error boundary to create project and dashboard routes

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -1,14 +1,13 @@
-import { type ReactElement } from "react";
+import { type ReactElement, lazy, Suspense } from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
   Routes,
+  useNavigate,
 } from "react-router-dom";
 import Calender from "src/pages/Dashboard/pages/Calenders";
-import CreateProject from "src/pages/CreateProject";
-import Dashboard from "src/pages/Dashboard";
 import ErrorPage from "src/pages/Error/ErrorPage";
 import Forgot from "src/pages/Forgot";
 import Home from "src/pages/Dashboard/pages/Main";
@@ -19,8 +18,14 @@ import Root from "./Root";
 import Signup from "src/pages/Signup";
 import SignupSuccess from "src/pages/SuccessfulScreen";
 import { CalenderContextProvider } from "src/context/CalenderContext";
+import { ErrorBoundary } from "react-error-boundary";
+
+const CreateProject = lazy(async () => await import("src/pages/CreateProject"));
+const Dashboard = lazy(async () => await import("src/pages/Dashboard"));
 
 const Routers = (): ReactElement => {
+  const nav = useNavigate();
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
@@ -29,24 +34,40 @@ const Routers = (): ReactElement => {
         <Route path="signup" element={<Signup />} />
         <Route path="forgot" element={<Forgot />} />
         <Route path="success" element={<SignupSuccess />} />
-        <Route path="create-project" element={<CreateProject />} />
+        <Route
+          path="create-project"
+          element={
+            <ErrorBoundary
+              FallbackComponent={ErrorPage}
+              onReset={() => {
+                nav("/");
+              }}
+            >
+              <Suspense fallback="loading">
+                <CreateProject />
+              </Suspense>
+            </ErrorBoundary>
+          }
+        />
         <Route
           path="dashboard/*"
           element={
-            <Dashboard>
-              <Routes>
-                <Route path="home" element={<Home />} />
-                <Route path="tasks" element={<ProjectTasks />} />
-                <Route
-                  path="calender"
-                  element={
-                    <CalenderContextProvider>
-                      <Calender />
-                    </CalenderContextProvider>
-                  }
-                />
-              </Routes>
-            </Dashboard>
+            <Suspense fallback="loading">
+              <Dashboard>
+                <Routes>
+                  <Route path="home" element={<Home />} />
+                  <Route path="tasks" element={<ProjectTasks />} />
+                  <Route
+                    path="calender"
+                    element={
+                      <CalenderContextProvider>
+                        <Calender />
+                      </CalenderContextProvider>
+                    }
+                  />
+                </Routes>
+              </Dashboard>
+            </Suspense>
           }
         />
       </Route>,
